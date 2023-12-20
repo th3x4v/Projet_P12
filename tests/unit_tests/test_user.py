59046c4a1ded1a2f33abe19317f28c6a1f083ac5
Xavier Lonjon <xavierlonjon@yahoo.fr>
2023-12-20 09:26:55 -0600

    user features completed + start of user tests

--- tests/unit_tests/test_user.py
+++ tests/unit_tests/test_user.py
@@ -5,19 +5,20 @@ from epic.models.models import User, Role  # Import the Role class
 from epic.cli.user_cli import app
 from typer.testing import CliRunner
 from epic.cli.auth_cli import authenticated_command
+import os
 
 
 runner = CliRunner()
 
 
-def test_create_user(
-    mock_authenticated_command, mocker, mock_user_info, mock_get_input
-):
+def test_create_user(mock_user_info, mock_get_input, setup_database):
     """
     Test the create_user function
     """
+    os.environ["TESTING"] = "True"
 
     result = runner.invoke(app, ["create"])
 
     assert result.exit_code == 0
     assert "User test_user created successfully." in result.output
+    del os.environ["TESTING"]
